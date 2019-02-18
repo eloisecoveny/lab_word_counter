16b4ed999cc2a352a4cdd936b8d8246771c52cb5
Eloise Coveny <eloisecoveny@gmail.com>
2019-02-18 19:18:30 +0000

    added some styling. Cannot center button

--- src/models/specs/word_counter_spec.js
+++ src/models/specs/word_counter_spec.js
@@ -3,8 +3,6 @@ const WordCounter = require('../WordCounter.js');
 
 describe('WordCounter', function () {
 
-  let word_counter;
-
   it('should count the number of words', function(){
     const actual = WordCounter.wordCount("This is a test string");
     assert.strictEqual(actual, 5);
